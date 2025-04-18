680e256cf1571cd5c6e788d2223ddf208580be5b
alabmc <alanbmc00@gmail.com>
2025-04-18 17:04:15 -0400

    feat: adicionando metodo para consumir llm em servidor

--- agents_airbnb.py
+++ agents_airbnb.py
@@ -1,5 +1,15 @@
 from praisonaiagents import Agent, MCP
+import os
+from langchain_community.llms import Ollama
+OLLAMA_BASE_URL = os.getenv("http://192.168.1.7:11434/", "http://localhost:11434")
+LOCAL_MODEL_NAME = "qwen2.5:0.5b"
 
+local_llm = Ollama(
+        model=LOCAL_MODEL_NAME,
+        base_url=OLLAMA_BASE_URL
+        # Add other parameters if needed, e.g., temperature, top_k, etc.
+        # temperature=0.7
+    )
 search_agent = Agent(
     instructions="""Voce deve me ajudar a encontrar uma casa para alugar no Airbnb""",
     llm="ollama/gemma3:1b",
